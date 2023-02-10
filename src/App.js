14eccd778eb7354e288772edf0d2ead2e6863a51
Justin Croonenberghs <justin@dataspacepublishing.com>
2023-02-10 17:36:53 -0600

    First working skeleton (auth, Tailwinds CSS) - basic auth deployed and tested - basic Tailwinds setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import Login from './components/Login';
+import Register from './components/Register';
+import ConfirmRegister from './components/ConfirmRegister';
+import PrivateRoute from './components/PrivateRoute';
+
 import './App.css';
+import './tailwind.generated.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Router>
+          <Switch>
+            <PrivateRoute path="/home">
+            </PrivateRoute>
+            <Route component={ConfirmRegister} path="/confirm-register" />
+            <Route component={Login} path="/log-in" />
+            <Route component={Register} path="/" />
+          </Switch>
+        </Router>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
